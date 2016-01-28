73952e262203f52fe53dc7ff6f9e2515d5b69fbb
Mary Doyle <mndoyle@ucdavis.edu>
2016-01-28 09:43:29 -0800

    Initial commit

---
+++ ex3.js
@@ -0,0 +1,398 @@
+//Javascript
+
+//check that the html is loaded
+$(document).ready(function() {
+  console.log('ready'),
+      
+    $('p').hide();
+    $('h5#des').hide();
+    $('h5#random').hide();
+    $('h5#coffeeTime').hide();
+    $('h5#lacrosse').hide();
+    $('#newimageA').hide();
+    $('#newimageD').hide();
+    $('#newimageF').hide();
+    $('#newimageG').hide();
+    $('#newimageH').hide();
+    $('#newimageJ').hide();
+    $('#newimageT').hide();
+    $('#newimageU').hide();
+    $('#newimageC').hide();
+    $('#newimageE').hide();
+    $('#newimageK').hide();
+    $('#newimageL').hide();
+    $('#newimageY').hide();
+    $('#newimageB').hide();
+    $('#newimageI').hide();
+    $('#newimageS').hide();
+    $('#newimageM').hide();
+    $('#newimageN').hide();
+    $('#newimageO').hide();
+    $('#newimageP').hide();
+    $('#newimageQ').hide();
+    $('#newimageR').hide();
+    $('#newimageV').hide();
+    $('#newimageW').hide();
+    $('#newimageX').hide();
+    
+    
+    
+    $('#activityC').hover(function() { 
+      $('#activityC p').show();
+        
+    }, function() { 
+      $('#activityC p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityA').hover(function() { 
+        $('#activityA p').show();
+    }, function() { 
+      $('#activityA p').hide();
+    });
+    
+    $('#activityB').hover(function() { 
+        $('#activityB p').show();
+        
+    }, function() { 
+      $('#activityB p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityD').hover(function() { 
+        $('#activityD p').show();
+    }, function() { 
+      $('#activityD p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityE').hover(function() { 
+        $('#activityE p').show();
+    }, function() { 
+      $('#activityE p').hide();
+    });
+    
+     $('#activityF').hover(function() { 
+        $('#activityF p').show();
+        
+    }, function() { 
+      $('#activityF p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityG').hover(function() { 
+        $('#activityG p').show();
+    }, function() { 
+      $('#activityG p').hide();
+    });
+    
+    $('#activityH').hover(function() { 
+        $('#activityH p').show();
+        
+    }, function() { 
+      $('#activityH p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityI').hover(function() { 
+        $('#activityI p').show();
+    }, function() { 
+      $('#activityI p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityJ').hover(function() { 
+        $('#activityJ p').show();
+    }, function() { 
+      $('#activityJ p').hide();
+    });
+    
+    
+    $('#activityK').hover(function() { 
+        $('#activityK p').show();
+        
+    }, function() { 
+      $('#activityK p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityL').hover(function() { 
+        $('#activityL p').show();
+    }, function() { 
+      $('#activityL p').hide();
+    });
+    
+    $('#activityM').hover(function() { 
+        $('#activityM p').show();
+        
+    }, function() { 
+      $('#activityM p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityN').hover(function() { 
+        $('#activityN p').show();
+    }, function() { 
+      $('#activityN p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityO').hover(function() { 
+        $('#activityO p').show();
+    }, function() { 
+      $('#activityO p').hide();
+    });
+    
+    $('#activityP').hover(function() { 
+        $('#activityP p').show();
+        
+    }, function() { 
+      $('#activityP p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityQ').hover(function() { 
+        $('#activityQ p').show();
+    }, function() { 
+      $('#activityQ p').hide();
+    });
+    
+    $('#activityR').hover(function() { 
+        $('#activityR p').show();
+        
+    }, function() { 
+      $('#activityR p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityS').hover(function() { 
+        $('#activityS p').show();
+    }, function() { 
+      $('#activityS p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityT').hover(function() { 
+        $('#activityT p').show();
+    }, function() { 
+      $('#activityT p').hide();
+    });
+     $('#activityU').hover(function() { 
+        $('#activityU p').show();
+        
+    }, function() { 
+      $('#activityU p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityV').hover(function() { 
+        $('#activityV p').show();
+    }, function() { 
+      $('#activityV p').hide();
+    });
+    
+    $('#activityW').hover(function() { 
+        $('#activityW p').show();
+        
+    }, function() { 
+      $('#activityW p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityX').hover(function() { 
+        $('#activityX p').show();
+    }, function() { 
+      $('#activityX p').hide();
+    });
+    
+    $('p').hide();
+    $('#activityY').hover(function() { 
+        $('#activityY p').show();
+    }, function() { 
+      $('#activityY p').hide();
+    });
+    
+    
+    
+    $('h6#design').click(function() {
+      console.log('click');
+        $('#newimageA').show();
+        $('#activityA p').show();
+        $('#newimageD').show();
+        $('#activityD p').show();
+        $('#newimageF').show();
+        $('#activityF p').show();
+        $('#newimageG').show();
+        $('#activityG p').show();
+        $('#newimageH').show();
+        $('#activityH p').show();
+        $('#newimageJ').show();
+        $('#activityJ p').show();
+        $('#newimageT').show();
+        $('#activityT p').show();
+        $('#newimageU').show();
+        $('#activityU p').show();
+      
+      return false;
+    
+    });
+    
+     $('img#designB').click(function() {
+      console.log('click');
+        $('#newimageA').hide();
+        $('#activityA p').hide();
+        $('#newimageD').hide();
+        $('#activityD p').hide();
+        $('#newimageF').hide();
+        $('#activityF p').hide();
+        $('#newimageG').hide();
+        $('#activityG p').hide();
+        $('#newimageH').hide();
+        $('#activityH p').hide();
+        $('#newimageJ').hide();
+        $('#activityJ p').hide();
+        $('#newimageT').hide();
+        $('#activityT p').hide();
+        $('#newimageU').hide();
+        $('#activityU p').hide();
+      return false;
+    
+    });
+    
+     
+    $('h6#blah').click(function() {
+      console.log('click');
+        $('#newimageC').show();
+        $('#activityC p').show();
+        $('#newimageE').show();
+        $('#activityE p').show();
+        $('#newimageK').show();
+        $('#activityK p').show();
+        $('#newimageL').show();
+        $('#activityL p').show();
+        $('#newimageY').show();
+        $('#activityY p').show();
+      return false;
+    
+    });
+    
+    $('img#designBlah').click(function() {
+      console.log('click');
+        $('#newimageC').hide();
+        $('#activityC p').hide();
+        $('#newimageE').hide();
+        $('#activityE p').hide();
+        $('#newimageK').hide();
+        $('#activityK p').hide();
+        $('#newimageL').hide();
+        $('#activityL p').hide();
+        $('#newimageY').hide();
+        $('#activityY p').hide();
+      return false;
+
+    });
+    
+    $('h6#coffee').click(function() {
+      console.log('click');
+        $('#newimageB').show();
+        $('#activityB p').show();
+        $('#newimageI').show();
+        $('#activityI p').show();
+        $('#newimageS').show();
+        $('#activityS p').show();
+      return false;
+    
+    });
+     
+    $('img#designCoffee').click(function() {
+      console.log('click');
+        $('#newimageB').hide();
+        $('#activityB p').hide();
+        $('#newimageI').hide();
+        $('#activityI p').hide();
+        $('#newimageS').hide();
+        $('#activityS p').hide();
+      return false;
+    
+    });
+    
+      $('h6#werkIt').click(function() {
+      console.log('click');
+        $('#newimageM').show();
+        $('#activityM p').show();
+        $('#newimageN').show();
+        $('#activityN p').show();
+        $('#newimageO').show();
+        $('#activityO p').show();
+        $('#newimageP').show();
+        $('#activityP p').show();
+        $('#newimageQ').show();
+        $('#activityQ p').show();
+        $('#newimageR').show();
+        $('#activityR p').show();
+        $('#newimageV').show();
+        $('#activityV p').show();
+        $('#newimageW').show();
+        $('#activityW p').show();
+        $('#newimageX').show();
+        $('#activityX p').show();
+
+      return false;
+    
+    });
+    
+    
+      $('img#lax').click(function() {
+      console.log('click');
+        $('#newimageM').hide();
+        $('#activityM p').hide();
+        $('#newimageN').hide();
+        $('#activityN p').hide();
+        $('#newimageO').hide();
+        $('#activityO p').hide();
+        $('#newimageP').hide();
+        $('#activityP p').hide();
+        $('#newimageQ').hide();
+        $('#activityQ p').hide();
+        $('#newimageR').hide();
+        $('#activityR p').hide();
+        $('#newimageV').hide();
+        $('#activityV p').hide();
+        $('#newimageW').hide();
+        $('#activityW p').hide();
+        $('#newimageX').hide();
+        $('#activityX p').hide();
+
+      return false;
+    
+    });
+    
+     $('h6#design').hover(function() { 
+      $('h5#des').show();
+        
+    }, function() { 
+      $('h5#des').hide();
+    });
+    
+    $('h6#blah').hover(function() { 
+      $('h5#random').show();
+        
+    }, function() { 
+      $('h5#random').hide();
+    });
+    
+    $('h6#coffee').hover(function() { 
+      $('h5#coffeeTime').show();
+        
+    }, function() { 
+      $('h5#coffeeTime').hide();
+    });
+    
+    $('h6#werkIt').hover(function() { 
+      $('h5#lacrosse').show();
+        
+    }, function() { 
+      $('h5#lacrosse').hide();
+    });
+    
+});
